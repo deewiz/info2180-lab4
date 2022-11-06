040ae265904ccb977a50b5524e468b120c33b318
Darrell Morgan <darrell.morgan02.dm@gmail.com>
2022-11-05 21:26:10 -0500

    made changes to js

--- app.js
+++ app.js
@@ -32,3 +32,40 @@ function mystarter(){
                 //alert(`Superheroes List \n ${data}`);
             })
             .catch(error => console.log('There was an error: ' + error));
+        }else{
+            //do
+            console.log("This is 2");
+            
+            fetch("superheroes.php", {
+                method: 'POST',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify(hero_form)
+            })
+            .then(response => {
+                if (response.ok) {
+                    return response.text()
+                } else {
+                    return Promise.reject('something went wrong!')
+                }
+            })
+            .then(data => {
+                console.log(data);
+                var hero = JSON.parse(data);
+                console.log(hero);
+                var hrname = hero["name"];
+                var shalias = "A.K.A  " + hero["alias"];
+                var biog = hero["biography"];
+                console.log(hrname);
+                console.log(shalias);
+                console.log(biog);
+                message.innerHTML = "";
+                heroname.innerHTML = hrname;
+                alias.innerHTML = shalias;
+                biography.innerHTML = biog;
+            });
+        }     
+    });
+}
+
+
+
